cea450701c85cd0c4480af0fe9077bb3ec45fc85
sjh <songjignhe@163.com>
2018-01-23 19:58:33 +0800

    add comparison test

--- src/test/java/org/act/temporalProperty/impl/index/BuildAndQueryTest.java
+++ src/test/java/org/act/temporalProperty/impl/index/BuildAndQueryTest.java
@@ -1,6 +1,5 @@
 package org.act.temporalProperty.impl.index;
 
-import com.google.common.primitives.Ints;
 import org.act.temporalProperty.TemporalPropertyStore;
 import org.act.temporalProperty.TemporalPropertyStoreFactory;
 import org.act.temporalProperty.impl.InternalKey;
@@ -60,17 +59,39 @@ public class BuildAndQueryTest {
 
     @Test
     public void main() throws Throwable {
-        IndexQueryRegion condition = new IndexQueryRegion(18300, 27000);
+        queryByIndex(18300, 27000, 0, 200);
+        queryByIter( 18300, 27000, 0, 200);
+
+//        for(int time=0; time<=18300; time+=100){
+//            for(int value=0; value<=400; value+=20){
+//
+//            }
+//        }
+    }
+
+    private List<Long> queryByIndex(int timeMin, int timeMax, int valueMin, int valueMax){
+        IndexQueryRegion condition = new IndexQueryRegion(timeMin, timeMax);
         Slice minValue = new Slice(4);
-        minValue.setInt(0, 0);
+        minValue.setInt(0, valueMin);
         Slice maxValue = new Slice(4);
-        maxValue.setInt(0, 200);
+        maxValue.setInt(0, valueMax);
         condition.add(new PropertyValueInterval(1, minValue, maxValue, IndexValueType.INT));
         List<Long> result = store.getEntities(condition);
         log.info("{}", result.size());
-//        for(Long eid : result){
-//
-//        }
+        return result;
+    }
+
+    private List<Long> queryByIter(int timeMin, int timeMax, int valueMin, int valueMax){
+        List<Long> result = new ArrayList<>();
+        for(Long entityId : roadIdMap.values()){
+            try {
+                store.getRangeValue(entityId, 1, timeMin, timeMax, new EntityIdCallBack(timeMin, timeMax, valueMin, valueMax));
+            }catch (StopLoopException e){
+                result.add(entityId);
+            }
+        }
+        log.info("{}", result.size());
+        return result;
     }
 
     @After
@@ -208,5 +229,42 @@ public class BuildAndQueryTest {
             deleteAllFilesOfDir(files[i]);
         }
         path.delete();
+    }
+
+    private class EntityIdCallBack extends RangeQueryCallBack {
+        private int timeMin, timeMax, valueMin, valueMax, lastTime = -1;
+        private boolean first = true;
+
+        public EntityIdCallBack(int timeMin, int timeMax, int valueMin, int valueMax) {
+            this.timeMin = timeMin;
+            this.timeMax = timeMax;
+            this.valueMin = valueMin;
+            this.valueMax = valueMax;
+        }
+
+        private boolean overlap(int t1min, int t1max, int t2min, int t2max){
+            return (t1min<=t2max && t2min<=t1max);
+        }
+
+        public void onCall(int time, Slice value) {
+            int val = value.getInt(0);
+            if(first){
+                first=false;
+                lastTime = val;
+            }else{
+                if(lastTime>=time) throw new RuntimeException("time not inc");
+                if(overlap(lastTime, time, timeMin, timeMax) &&
+                        (valueMin <= val && val <= valueMax)){
+                    throw new StopLoopException();
+                }
+            }
+        }
+        public void setValueType(String valueType) {}
+        public void onCallBatch(Slice batchValue) {}
+        public Object onReturn() {return null;}
+        public CallBackType getType() {return null;}
+    };
+
+    private class StopLoopException extends RuntimeException {
     }
 }
