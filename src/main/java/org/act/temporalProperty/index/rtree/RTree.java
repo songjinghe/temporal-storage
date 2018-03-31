a01aea084eb744d40e258dabaf9af1bc573c386f
sjh <songjignhe@163.com>
2018-03-31 22:42:04 +0800

    fix bug: RTree pack index node error.

--- src/main/java/org/act/temporalProperty/index/rtree/RTree.java
+++ src/main/java/org/act/temporalProperty/index/rtree/RTree.java
@@ -50,7 +50,7 @@ public class RTree {
     private RTreeNode packNode(List<RTreeNode> nodes){
         this.sortNodes(nodes);
         List<RTreeNode> upperLevelNodes = new ArrayList<>();
-        for(int i = 0; i<=nodes.size(); i+= bIndex) {
+        for(int i = 0; i<nodes.size(); i+= bIndex) {
             int end = (i+bIndex>nodes.size())? nodes.size() : i+bIndex;
             RTreeNode node = new RTreeIndexNode(nodes.subList(i, end), this.op);
             upperLevelNodes.add(node);
